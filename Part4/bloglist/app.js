30309397ed519d22e08dc1ccf45a97268f7d49e9
Riukim <m.cominelli29@gmail.com>
2024-01-24 10:10:37 +0100

    Refactored Bloglist backend
    Refactored the bloglist backend to adhere to Node.js best practices, solving Exercises 4.1.-4.2

--- Part4/bloglist/app.js
+++ Part4/bloglist/app.js
@@ -1,11 +1,38 @@
-require("dotenv").config({ path: "./mongodb_uri.env" })
 
+const config = require("./utils/config")
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const blogsRouter = require("./controllers/blogs")
+const middleware = require("./utils/middleware")
+const logger = require("./utils/logger")
 const mongoose = require("mongoose")
+mongoose.set("strictQuery", false)
 
-const blogSchema = new mongoose.Schema({
+logger.info("connecting to", config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+	.then(() => {
+		logger.info("connected to MongoDB")
+	})
+	.catch((error) => {
+		logger.error("error.connecting to MongoDB:", error.message)
+	})
+
+app.use(cors())
+app.use(express.static("build"))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use("/api/blogs", blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
+
+
+/* const blogSchema = new mongoose.Schema({
 	title: String,
 	author: String,
 	url: String,
@@ -50,7 +77,20 @@ app.post("/api/blogs", (request, response) => {
 		})
 })
 
+app.get("/api/blogs/:id", (request, response, next) => {
+	Blog
+		.findById(request.params.id)
+		.then(blog => {
+			if (blog) {
+				response.json(blog)
+			} else {
+				response.status(400).end()
+			}
+		})
+		.catch(error => next(error))
+})
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
-})
+}) */
