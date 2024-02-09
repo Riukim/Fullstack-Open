cb3928d47b1da0abe7bde339275f2a5aa2f9902c
Riukim <m.cominelli29@gmail.com>
2024-02-09 11:47:44 +0100

    Changed filterReducer to use createSlice

--- Part6/redux-anecdotes/src/reducers/filterReducer.js
+++ Part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,4 +1,21 @@
-const filterReducer = (state = "", action) => {
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = ""
+
+const filterSlice = createSlice({
+    name: "filter",
+    initialState,
+    reducers: {
+        filterChange(state, action) {
+            return action.payload
+        }
+    }
+})
+
+export const { filterChange } = filterSlice.actions
+export default filterSlice.reducer
+
+/* const filterReducer = (state = "", action) => {
     switch (action.type) {
         case "SET_FILTER":
             return action.payload
@@ -14,4 +31,4 @@ export const filterChange = filter => {
     }
 }
 
-export default filterReducer
+export default filterReducer */
