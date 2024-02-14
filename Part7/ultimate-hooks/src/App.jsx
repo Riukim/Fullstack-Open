17be32ce011cd2971bed3ab61289f915fb568ecd
Riukim <m.cominelli29@gmail.com>
2024-02-14 12:09:20 +0100

    Added custom hook useResource

--- Part7/ultimate-hooks/src/App.jsx
+++ Part7/ultimate-hooks/src/App.jsx
@@ -18,10 +18,26 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    const getAll = async () => {
+      try {
+        const response = await axios.get(baseUrl)
+        return setResources(response.data)
+      } catch (error) {
+        console.error("Error fetching data:", error)
+      }
+    }
+    getAll()
+  }, [baseUrl])
 
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+    try {
+      const response = await axios.post(baseUrl, resource)
+      console.log(resource);
+      setResources([...resources, response.data])
+    } catch (error) {
+      console.error("Error creating resource:", error)
+    }
   }
 
   const service = {
